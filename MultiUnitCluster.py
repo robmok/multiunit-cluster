5d1bca759cb5a163456e5a93f934a46f5cfd8b7b
robmok <rob1031@gmail.com>
2021-07-20 23:59:16 +0100

    Update MultiUnitCluster.py

--- MultiUnitCluster.py
+++ MultiUnitCluster.py
@@ -138,12 +138,9 @@ class MultiUnitCluster(nn.Module):
 
         # compute attention-weighted dist & activation (based on similarity)
         act = self._compute_act(
-            dist, self.params['c'], self.params['p'])
+            dist, self.params['c'], self.params['p']).to(self.device)
 
-        act = act.to(self.device)
-
-        print(act.is_cuda)
-        print(self.winning_units.is_cuda)
+        print('x: {}'.format(act))
 
         units_output = act * self.winning_units.to(self.device)
 
