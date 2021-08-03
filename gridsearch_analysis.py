d8c37bcd40c22e340bddf61ca1d915817b05b958
robmok <rob1031@gmail.com>
2021-08-03 14:27:47 +0100

    Update gridsearch_analysis.py

--- gridsearch_analysis.py
+++ gridsearch_analysis.py
@@ -6,7 +6,6 @@ Created on Mon Aug  2 22:57:19 2021
 @author: robert.mok
 """
 
-
 import os
 # import numpy as np
 import torch
@@ -14,26 +13,79 @@ import matplotlib.pyplot as plt
 import pickle
 
 maindir = '/Users/robert.mok/Documents/Postdoc_cambridge_2020/'
-resdir = os.path.join(maindir, 'muc-shj-gridsearch/gsearch_k0.01_500units')
 
 k = 0.01
 n_units = 500
-iset = 0
 
-fn = os.path.join(resdir,
-                  'shj_gsearch_k{}_{}units_set{}.pkl'.format(k, n_units, iset))
+resdir = os.path.join(maindir, 'muc-shj-gridsearch/gsearch_k{}_{}units'.format(
+    k, n_units))
 
-# load - list: [nlls, pt_all, rec_all, seeds_all]
-open_file = open(fn, "rb")
-loaded_list = pickle.load(open_file)
-open_file.close()
+pts = []
+nlls = []
+recs = []
+seeds = []
+
+for iset in range(250):
+    fn = os.path.join(
+        resdir,
+        'shj_gsearch_k{}_{}units_set{}.pkl'.format(k, n_units, iset))
+
+    # load - list: [nlls, pt_all, rec_all, seeds_all]
+    open_file = open(fn, "rb")
+    loaded_list = pickle.load(open_file)
+    open_file.close()
+
+    nlls.extend(loaded_list[0])
+    pts.extend(loaded_list[1])
+    # recs.extend(loaded_list[2])
+    # seeds.extend(loaded_list[3])
 
 
-pts = torch.stack(loaded_list[1])
+pts = torch.stack(pts).T
+nlls = torch.stack(nlls)
+# recs = torch.stack(recs)
+# seeds = torch.stack(seeds)
+
+# %%
+
+# the human data from nosofsky, et al. replication
+shj = (
+    torch.tensor(
+        [[0.211, 0.025, 0.003, 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0.],
+         [0.378, 0.156, 0.083, 0.056, 0.031, 0.027, 0.028, 0.016,
+          0.016, 0.008, 0., 0.002, 0.005, 0.003, 0.002, 0.],
+         [0.459, 0.286, 0.223, 0.145, 0.081, 0.078, 0.063, 0.033,
+         0.023, 0.016, 0.019, 0.009, 0.008, 0.013, 0.009, 0.013],
+         [0.422, 0.295, 0.222, 0.172, 0.148, 0.109, 0.089, 0.062,
+          0.025, 0.031, 0.019, 0.025, 0.005, 0., 0., 0.],
+         [0.472, 0.331, 0.23, 0.139, 0.106, 0.081, 0.067,
+          0.078, 0.048, 0.045, 0.05, 0.036, 0.031, 0.027, 0.016, 0.014],
+         [0.498, 0.341, 0.284, 0.245, 0.217, 0.192, 0.192, 0.177,
+          0.172, 0.128, 0.139, 0.117, 0.103, 0.098, 0.106, 0.106]]).T
+    )
+
+# criteria for fit:
+
+# quantitative:
+# - compute SSE for each fit (already have nll)
+
+# qualitiative
+# - pattern - 1, 2, 3-4-5, 6 (where 3, 4, 5 can be in any order for now). all
+# points have to be faster (for now - maybe do 80% points if problem?)
+# -
+
 
 iparam = 0
-plt.plot(pts[iparam].T)
+
+sse = torch.sum(torch.square(pts[iparam].T.flatten() - shj.flatten()))
+
+
 
 # for iparam in range(10):
 #     plt.plot(pts[iparam].T)
 #     plt.show()
+
+
+
+
